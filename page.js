348273192d7bd1807c09cb61ee1ce4d21e759c87
GTechMason <GTechMason@gmail.com>
2024-08-03 12:40:05 -0400

    Update page.js

--- page.js
+++ page.js
@@ -1,51 +1,45 @@
-module.exports = {
-    // Inputs
-    fromField: '#from',
-    toField: '#to',
-    phoneNumberField: '#phone',
-    codeField: '#code',
-    // Buttons
-    callATaxiButton: 'button=Call a taxi',
-    phoneNumberButton: '//div[starts-with(text(), "Phone number")]',
-    nextButton: 'button=Next',
-    confirmButton: 'button=Confirm',
-    // Modals
-    phoneNumberModal: '.modal',
-    // Functions
-    fillAddresses: async function(from, to) {
-        const fromField = await $(this.fromField);
-        await fromField.setValue(from);
-        const toField = await $(this.toField);
-        await toField.setValue(to);
-        const callATaxiButton = await $(this.callATaxiButton);
-        await callATaxiButton.waitForDisplayed();
-        await callATaxiButton.click();
+exports.config = {
+
+    runner: 'local',
+
+    specs: [
+        './test/specs/**/*.js'
+    ],
+
+    exclude: [
+        // 'path/to/excluded/files'
+    ],
+
+    maxInstances: 10,
+
+    capabilities: [{
+        browserName: 'chrome'
+    }, {
+        browserName: 'firefox'
+    }],
+
+    logLevel: 'error',
+
+    bail: 0,
+
+  baseUrl: 'https://cnt-69b22599-c95e-46d0-81b7-cb8b3993602b.containerhub.tripleten-services.com',
+
+    waitforTimeout: 10000,
+ 
+    connectionRetryTimeout: 120000,
+   
+    connectionRetryCount: 3,
+   
+    services: ['visual', 'firefox-profile', 'intercept'],
+
+    framework: 'mocha',
+ 
+    reporters: ['spec'],
+
+
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
     },
-    fillPhoneNumber: async function(phoneNumber) {
-        const phoneNumberButton = await $(this.phoneNumberButton);
-        await phoneNumberButton.waitForDisplayed();
-        await phoneNumberButton.click();
-        const phoneNumberModal = await $(this.phoneNumberModal);
-        await phoneNumberModal.waitForDisplayed()
-        const phoneNumberField = await $(this.phoneNumberField);
-        await phoneNumberField.waitForDisplayed();
-        await phoneNumberField.setValue(phoneNumber);
-    },
-    submitPhoneNumber: async function(phoneNumber) {
-        await this.fillPhoneNumber(phoneNumber);
-        // we are starting interception of request from the moment of method call
-        await browser.setupInterceptor();
-        await $(this.nextButton).click();
-        // we should wait for response
-        // eslint-disable-next-line wdio/no-pause
-        await browser.pause(2000);
-        const codeField = await $(this.codeField);
-        // collect all responses
-        const requests = await browser.getRequests();
-        // use first response
-        await expect(requests.length).toBe(1)
-        const code = await requests[0].response.body.code
-        await codeField.setValue(code)
-        await $(this.confirmButton).click()
-    },
-};
+
+}
