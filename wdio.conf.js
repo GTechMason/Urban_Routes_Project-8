8de0309abfc156c7176975dc1fedb7c031b56b28
GTechMason <GTechMason@gmail.com>
2024-08-03 12:40:36 -0400

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,46 +1,45 @@
 exports.config = {
+
     runner: 'local',
+
     specs: [
         './test/specs/**/*.js'
     ],
+
     exclude: [
         // 'path/to/excluded/files'
     ],
+
     maxInstances: 10,
-    headless: true,
-    capabilities: [
-        {
-        maxInstances: 5,
-        browserName: 'chrome',
-        acceptInsecureCerts: true,
-        'goog:chromeOptions': {
-            args: ['headless', 'disable-gpu']
-        }
-    },
-    {
-        maxInstances: 5,
-        browserName: 'firefox',
-        acceptInsecureCerts: true,
-        'moz:firefoxOptions': {
-            args: ['-headless']
-        }
-    }
-],
+
+    capabilities: [{
+        browserName: 'chrome'
+    }, {
+        browserName: 'firefox'
+    }],
+
     logLevel: 'error',
+
     bail: 0,
-    baseUrl: 'https://cnt-70cff22f-1d99-41ec-bbfa-eb5625fbdfd2.containerhub.tripleten-services.com',
+
+  baseUrl: 'https://cnt-69b22599-c95e-46d0-81b7-cb8b3993602b.containerhub.tripleten-services.com',
+
     waitforTimeout: 10000,
+ 
     connectionRetryTimeout: 120000,
+   
     connectionRetryCount: 3,
-    services: [
-        'chromedriver', 
-        'geckodriver', 
-        'intercept', 
-    ],
+   
+    services: ['visual', 'firefox-profile', 'intercept'],
+
     framework: 'mocha',
+ 
     reporters: ['spec'],
+
+
     mochaOpts: {
         ui: 'bdd',
         timeout: 60000
     },
+
 }
